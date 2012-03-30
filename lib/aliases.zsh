db085b8a5fb0d22ddb7851b6d207519fb90c0a8e
Lonnon Foster <lonnon.foster@gmail.com>
2012-03-30 14:12:36 -0700

    Add functions for deleting backup, auto-save files
    Also swap -a for -A in ls aliases; no need to list . and .. directories.

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -10,6 +10,24 @@ alias h='fc -l 1'
 alias ls='ls -sFC --color=auto'
 alias ll='ls -l'
 alias l.='ls -d .*'
-alias la='ls -a'
-alias lla='ls -al'
+alias la='ls -A'
+alias lla='ls -Al'
 alias ll.='ls -ld .*'
+
+# Cleanup of vim and emacs backup, auto-save files
+rmb() {
+  { rm -f *~ } 2>/dev/null
+  { rm -f .*~ } 2>/dev/null
+}
+rrmb() {
+  { rm -f **/*~ } 2>/dev/null
+  { rm -f **/.*~ } 2>/dev/null
+}
+rmswp() {
+  { rm -f .*.swp } 2>/dev/null
+  { rm -f \#*\# } 2>/dev/null
+}
+rrmswp() {
+  { rm -f **/.*.swp } 2>/dev/null
+  { rm -f **/\#*\# } 2>/dev/null
+}
