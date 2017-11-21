89d74d5f517a5776d49c2db841811be101f42cb1
Lonnon Foster <lonnon.foster@gmail.com>
2017-11-21 13:43:44 -0800

    Remove deprecated GREP_OPTIONS

--- lib/grep.zsh
+++ lib/grep.zsh
@@ -2,5 +2,6 @@
 # Color grep results
 # Examples: http://rubyurl.com/ZXv
 #
-export GREP_OPTIONS='--color=auto'
+# Obsolete; replaced with alias grep='grep -n --color=auto'
+# export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;32'
